97d55fce35950bbcf2337e23ff6a2b7db01ef981
25tal <72045932+51talcol21@users.noreply.github.com>
2025-07-13 15:41:21 -0700

    Parse Input

--- rust_maze_solver/src/main.rs
+++ rust_maze_solver/src/main.rs
@@ -1,33 +1,44 @@
 use std::io::{self, BufRead, BufReader, Result};
 use std::fs::File;
 
+#[derive(Debug)]
 struct Point (usize, usize);
 
-fn parse_input(filename: &str) -> Result<()> {
+fn parse_input(filename: &str) -> Result<(Vec<Vec<u8>>, Point, Point)> {
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
 
-    let mut each_line = reader.lines();
-    let start_point_line = each_line.next().unwrap()?;
-    let end_point_line = each_line.next().unwrap()?;
+    let mut all_lines = reader.lines();
+    let start_point_line = all_lines.next().unwrap()?;
+    let end_point_line = all_lines.next().unwrap()?;
 
-    println!("--------------");
-    println!("{} : {}", start_point_line, end_point_line);
-    println!("{:?}", parse_point(&start_point_line));
-    println!("{:?}", parse_point(&end_point_line));
-    Ok(())
+    let start_point = parse_point(&start_point_line);
+    let end_point = parse_point(&end_point_line);
+
+    let mut maze = Vec::<Vec<u8>>::new();
+
+    for each_line in all_lines {
+        let row = each_line?.split_whitespace()
+            .filter(|s| s.chars().all(char::is_numeric))
+            .map(|s| s.parse::<u8>().unwrap())
+            .collect::<Vec<u8>>();
+        println!("{:?}", row);
+        maze.push(row);
+    }
+    Ok((maze, start_point.unwrap(), end_point.unwrap()))
 }
 
-fn parse_point(point_line: &str) -> Result<()> {
+fn parse_point(point_line: &str) -> Result<(Point)> {
     let coordinate: Vec<_> = 
         point_line.split(|c: char| c == '(' || c == ')' || c == ',')
+        .filter(|s| !s.is_empty() && s.chars().all(char::is_numeric))
+        .map(|s| s.parse::<usize>().unwrap())
         .collect();
-    println!("Coordinate: {:?}", coordinate);
-    Ok(())
+    Ok(Point (coordinate[0], coordinate[1]))
 }
 
 fn main() -> Result<()> {
-    // let (maze, start, goal) = parse_input("../../input.txt")?;
-    parse_input("../input.txt");
+    let (maze, start_point, end_point) = parse_input("../input.txt").unwrap();
+    println!("{:?} : {:?} : {:?}", maze, start_point, end_point);
     Ok(())
 }
