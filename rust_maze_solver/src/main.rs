8d6e3c415fb87941aeab9a20748b837de691d6b5
25tal <72045932+51talcol21@users.noreply.github.com>
2025-07-18 12:02:54 -0700

    BFS in Rust

--- rust_maze_solver/src/main.rs
+++ rust_maze_solver/src/main.rs
@@ -1,7 +1,9 @@
-use std::io::{self, BufRead, BufReader, Result};
+use std::io::{BufRead, BufReader, Result};
 use std::fs::File;
+use std::collections::HashMap;
+use std::collections::VecDeque;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, Hash, Eq, PartialEq)]
 struct Point (usize, usize);
 
 fn parse_input(filename: &str) -> Result<(Vec<Vec<u8>>, Point, Point)> {
@@ -28,7 +30,7 @@ fn parse_input(filename: &str) -> Result<(Vec<Vec<u8>>, Point, Point)> {
     Ok((maze, start_point.unwrap(), end_point.unwrap()))
 }
 
-fn parse_point(point_line: &str) -> Result<(Point)> {
+fn parse_point(point_line: &str) -> Result<Point> {
     let coordinate: Vec<_> = 
         point_line.split(|c: char| c == '(' || c == ')' || c == ',')
         .filter(|s| !s.is_empty() && s.chars().all(char::is_numeric))
@@ -37,8 +39,62 @@ fn parse_point(point_line: &str) -> Result<(Point)> {
     Ok(Point (coordinate[0], coordinate[1]))
 }
 
+fn bfs_directions(maze: Vec<Vec<u8>>, start_point: Point, end_point: Point) -> Option<Vec<String>> {
+    let rows = maze.len();
+    let cols = maze[0].len();
+
+    let mut visited = vec![vec![false; cols]; rows];
+    let mut parent_hash_map = HashMap::<Point, (Point, &str)>::new();
+    let mut queue = VecDeque::<Point>::new();
+
+    queue.push_back(start_point);
+    visited[start_point.0][start_point.1] = true;
+
+    let directions: Vec<((isize, isize), &str)> = vec![
+        ((-1, 0), "Up"),
+        ((0,1), "Right"),
+        ((0,-1), "Left"),
+        ((1, 0), "Down"),
+    ];
+
+    while let Some(Point(x,y)) = queue.pop_front() {
+        // Check goal state
+        println!("{:?}", (x, y));
+        if(x,y) == (end_point.0, end_point.1) {
+            let mut path: Vec<String> = Vec::new();
+            let mut current = end_point;
+
+            while let Some((prev_point, direction)) = parent_hash_map.get(&current) {
+                path.push(direction.to_string());
+                current = *prev_point;
+            }
+
+            path.reverse();
+            return Some(path);
+        }
+
+        for((dir_x, dir_y), direction) in &directions {
+            let new_x = x as isize + dir_x;
+            let new_y = y as isize + dir_y;
+            // If within the bounds of the maze
+            if new_x >= 0 && new_y >= 0 && new_x <= (rows - 1) as isize && new_y <= (cols - 1) as isize {
+                // If not visited
+                if !visited[new_x as usize][new_y as usize] && (maze[new_x as usize][new_y as usize] == 1 || maze[new_x as usize][new_y as usize] == 0) {
+                    visited[new_x as usize][new_y as usize] = true;
+                    parent_hash_map.insert(Point (new_x as usize, new_y as usize), (Point (x, y), direction));
+                    queue.push_back(Point (new_x as usize, new_y as usize));
+                }
+            }
+        }
+    }
+
+    None
+}
+
 fn main() -> Result<()> {
     let (maze, start_point, end_point) = parse_input("../input.txt").unwrap();
     println!("{:?} : {:?} : {:?}", maze, start_point, end_point);
+    let path_directions = bfs_directions(maze, start_point, end_point);
+    println!("{:?}", path_directions);
     Ok(())
 }
